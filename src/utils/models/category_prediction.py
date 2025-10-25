e2496f904c8e9c0e84b5e3bf4319455294a55846
edgardanielgd <edgardanielgd123@gmail.com>
2025-10-25 14:26:14 -0500

    feat: Enhanced modeling versioning

--- src/utils/models/category_prediction.py
+++ src/utils/models/category_prediction.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from src.utils.models.base import BaseModel
 import tensorflow_io as tfio
-import pyarrow.parquet as pq
+import os
 
 class CategoryPredictionModel(BaseModel):
     def __init__(self, model_name: str, metastore_path: str, num_classes: int = 5, tracking_uri: str = "file:///tmp/mlruns"):
@@ -17,7 +17,6 @@ class CategoryPredictionModel(BaseModel):
         
         def split_xy(row):
             target_cols = [k for k in row.keys() if k in target_columns]
-            # Convert targets to float32 for categorical_crossentropy
             y = tf.stack([tf.cast(row[k], tf.float32) for k in target_cols], axis=-1)
             feature_cols = [k for k in row.keys() if k not in target_cols]
             x = tf.stack([tf.cast(row[k], tf.float32) for k in feature_cols], axis=-1)
@@ -46,19 +45,55 @@ class CategoryPredictionModel(BaseModel):
             spark, dst_path, dataset, train_ratio, val_ratio, **dataset_kwargs
         )
 
+    def make_model_callbacks(self) -> list:
+        os.makedirs(f"{self.metastore_path}/warehouse/gold.premodeling/{self.model_name}/model_checkpoints", exist_ok=True)
+
+        callbacks = [
+            tf.keras.callbacks.EarlyStopping(
+                monitor='val_loss',
+                patience=7,
+                restore_best_weights=True,
+                verbose=1
+            ),
+            tf.keras.callbacks.ReduceLROnPlateau(
+                monitor='val_loss',
+                factor=0.5,
+                patience=2,
+                min_lr=1e-7,
+                verbose=1
+            ),
+            tf.keras.callbacks.ModelCheckpoint(
+                filepath=f"{self.metastore_path}/warehouse/gold.premodeling/{self.model_name}/model_checkpoints/rating_model_epoch_{{epoch:02d}}.h5",
+                monitor='val_loss',
+                save_best_only=True,
+                save_weights_only=False,
+                verbose=1
+            )
+        ]
+
+        return callbacks
+
     def _build_model(self) -> tf.keras.Model:
         model = tf.keras.Sequential([
             tf.keras.layers.Input(shape=(self.input_shape,)),
+            tf.keras.layers.Dense(1024, activation='relu'),
+            tf.keras.layers.Dropout(0.4),
             tf.keras.layers.Dense(512, activation='relu'),
             tf.keras.layers.Dropout(0.4),
             tf.keras.layers.Dense(256, activation='relu'),
+            tf.keras.layers.Dense(256, activation='relu'),
             tf.keras.layers.Dropout(0.3),
+            tf.keras.layers.Dense(128, activation='relu'),
             tf.keras.layers.Dense(128, activation='relu'),
             tf.keras.layers.Dropout(0.2),
             tf.keras.layers.Dense(64, activation='relu'),
+            tf.keras.layers.Dense(64, activation='relu'),
             tf.keras.layers.Dropout(0.1),
             tf.keras.layers.Dense(32, activation='relu'),
+            tf.keras.layers.Dense(32, activation='relu'),
             tf.keras.layers.Dropout(0.05),
+            tf.keras.layers.Dense(16, activation='relu'),
+            tf.keras.layers.Dense(8, activation='relu'),
             tf.keras.layers.Dense(self.num_classes, activation='softmax')
         ])
         
